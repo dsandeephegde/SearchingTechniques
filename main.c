feb060a25d23971df4a2c5bcfe5ae164027eff54
Sandeep Hegde <dsandeephegde@gmail.com>
2018-03-29 13:03:16 -0400

    Refactored and optimized code

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <sys/time.h>
-#include <memory.h>
+#include <string.h>
 #include <stdlib.h>
 
 int binarySearch(int list[], int searchTerm, long first, long last)
@@ -20,39 +20,37 @@ int binarySearch(int list[], int searchTerm, long first, long last)
     return 0;
 }
 
-int binarySearchDisk(FILE* list, int searchTerm, long first, long last)
+int binarySearchOnDisk(FILE *file, int searchTerm, long first, long last)
 {
     long middle = (first + last) / 2;
     if(first <= last) {
         int middleElement;
-        fseek( list, middle * sizeof( int ), SEEK_SET );
-        fread(&middleElement, sizeof(int), 1, list);
+        fseek( file, middle * sizeof( int ), SEEK_SET );
+        fread(&middleElement, sizeof(int), 1, file);
         if(searchTerm == middleElement) {
             return 1;
         }
         else if(searchTerm < middleElement){
-            return binarySearchDisk(list, searchTerm, first, middle - 1);
+            return binarySearchOnDisk(file, searchTerm, first, middle - 1);
         }
         else {
-            return binarySearchDisk(list, searchTerm, middle + 1, last);
+            return binarySearchOnDisk(file, searchTerm, middle + 1, last);
         }
     }
     return 0;
 }
 
-long getNumberOfIntegers(char *fileName) {
-    long length = 0, temp;
-    FILE *file = fopen(fileName, "rb");
-    while (fread( &temp, sizeof(int), 1, file)) {
-        length++;
-    }
+long getNumberOfIntegers(FILE *file) {
+    fseek( file, 0, SEEK_END );
+    int length = ftell(file) / sizeof(int);
+    fseek( file, 0, SEEK_SET );
     return length;
 }
 
 int main(int argc, char *argv[]) {
     struct timeval startTime, endTime, execTime;
     FILE *keyFile, *seekFile;
-    int key, seek, i, j;
+    int key, i, j;
 
     if(argc != 4) {
         printf("Usage: assn_1 searchMode keyFileName seekFileName\n");
@@ -62,28 +60,26 @@ int main(int argc, char *argv[]) {
     searchMode = argv[1];
     keyFileName = argv[2];
     seekFileName = argv[3];
-    long keysLength = getNumberOfIntegers(keyFileName), seeksLength = getNumberOfIntegers(seekFileName);
-    int keys[keysLength], seeks[seeksLength], hits[seeksLength];
 
     keyFile = fopen(keyFileName, "rb");
     seekFile = fopen(seekFileName, "rb");
+
+    long keysLength = getNumberOfIntegers(keyFile), seeksLength = getNumberOfIntegers(seekFile);
+    int keys[keysLength], seeks[seeksLength], hits[seeksLength];
 
     if(keyFile == NULL || seekFile == NULL) {
         printf("Please enter the correct file");
         exit(1);
     }
 
-    for (i = 0; fread(&seek, sizeof(int), 1, seekFile); i++) {
-        seeks[i] = seek;
+    fread(seeks, sizeof(int), (size_t) seeksLength, seekFile);
+    for (i = 0; i < seeksLength; ++i) {
         hits[i] = 0;
     }
 
+    gettimeofday(&startTime, NULL);
     if (strcmp(searchMode, "--mem-lin") == 0) {
-        gettimeofday(&startTime, NULL);
-        for (i = 0; fread(&key, sizeof(int), 1, keyFile); i++) {
-            keys[i] = key;
-        }
-
+        fread(keys, sizeof(int), (size_t) keysLength, keyFile);
         for (i = 0; i < seeksLength; i++) {
             for (j = 0; j < keysLength; j++) {
                 if (keys[j] == seeks[i]) {
@@ -91,10 +87,8 @@ int main(int argc, char *argv[]) {
                 }
             }
         }
-        gettimeofday(&endTime, NULL);
     }
     else if (strcmp(searchMode, "--disk-lin") == 0) {
-        gettimeofday(&startTime, NULL);
         for (i = 0; i < seeksLength; i++) {
             fseek( keyFile, 0, SEEK_SET );
             while (fread(&key, sizeof(int), 1, keyFile)) {
@@ -103,31 +97,24 @@ int main(int argc, char *argv[]) {
                 }
             }
         }
-        gettimeofday(&endTime, NULL);
     }
     else if (strcmp(searchMode, "--mem-bin") == 0) {
-        gettimeofday(&startTime, NULL);
-        for (i = 0; fread(&key, sizeof(int), 1, keyFile); i++) {
-            keys[i] = key;
-        }
+        fread(keys, sizeof(int), (size_t) keysLength, keyFile);
 
         for (i = 0; i < seeksLength; i++) {
             hits[i] = binarySearch(keys, seeks[i], 0, keysLength - 1 );
         }
-        gettimeofday(&endTime, NULL);
     }
     else if (strcmp(searchMode, "--disk-bin") == 0) {
-        gettimeofday(&startTime, NULL);
         for (i = 0; i < seeksLength; i++) {
-            fseek( keyFile, sizeof( int ), SEEK_SET );
-            hits[i] = binarySearchDisk(keyFile, seeks[i], 0, keysLength - 1 );
+            hits[i] = binarySearchOnDisk(keyFile, seeks[i], 0, keysLength - 1);
         }
-        gettimeofday(&endTime, NULL);
     }
     else {
         printf("Please enter the correct option\n");
         exit(1);
     }
+    gettimeofday(&endTime, NULL);
 
     for (i = 0; i < seeksLength; ++i) {
         printf("%12d: ", seeks[i]);
